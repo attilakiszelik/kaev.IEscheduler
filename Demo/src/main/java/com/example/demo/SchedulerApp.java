481261c64a0d97288cf1235465af95110edb7168
A <attila.kiszelik@gmail.com>
2021-01-29 11:37:35 +0100

    rename

--- Demo/src/main/java/com/example/demo/SchedulerApp.java
+++ Demo/src/main/java/com/example/demo/SchedulerApp.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class SchedulerApp {
 
 	public static void main(String[] args) {
 	
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SchedulerApp.class, args);
 	
 	}
 
