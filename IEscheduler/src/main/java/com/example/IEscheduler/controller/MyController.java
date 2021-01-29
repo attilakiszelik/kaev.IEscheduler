efe981a5c47933838c96d68d6bdb148e6724e907
A <attila.kiszelik@gmail.com>
2021-01-29 19:39:23 +0100

    add controller and service packages

--- IEscheduler/src/main/java/com/example/IEscheduler/controller/MyController.java
+++ IEscheduler/src/main/java/com/example/IEscheduler/controller/MyController.java
@@ -1,8 +1,10 @@
-package com.example.IEscheduler;
+package com.example.IEscheduler.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.IEscheduler.service.MyService;
 
 @RestController
 public class MyController {
--- IEscheduler/src/main/java/com/example/IEscheduler/service/MyService.java
+++ IEscheduler/src/main/java/com/example/IEscheduler/service/MyService.java
@@ -1,4 +1,4 @@
-package com.example.IEscheduler;
+package com.example.IEscheduler.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
