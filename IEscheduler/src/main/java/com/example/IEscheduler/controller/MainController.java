7f382cdd24ec1fd0adce24ed2e95a4f360c9cbb8
A <attila.kiszelik@gmail.com>
2021-02-11 19:04:38 +0100

    build in GetMapping / PostMapping with error :D

--- IEscheduler/src/main/java/com/example/IEscheduler/controller/MainController.java
+++ IEscheduler/src/main/java/com/example/IEscheduler/controller/MainController.java
@@ -1,10 +1,14 @@
 package com.example.IEscheduler.controller;
 
+import com.example.IEscheduler.domain.Vehicle;
 import com.example.IEscheduler.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,16 +21,22 @@ public class MainController {
 		this.mainService = mainService;
 	}
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String vehicles(Model model){
 		model.addAttribute("vehicles", mainService.getVehicles());
 		return "vehicles";
 	}
 	
-	@RequestMapping("/new")
+	@GetMapping("/new")
 	public String newVehicle(Model model){
 		mainService.newVehicle(false, "ZZZ-111", "IVECO", "nagy", 2020, 1);
 		model.addAttribute("vehicles", mainService.getVehicles());
+		return "vehicles";
+	}
+	
+	@PostMapping("/new")
+	public String greetingSubmit(@ModelAttribute Vehicle vehicle, Model model) {
+		model.addAttribute("vehicles", vehicle);
 		return "vehicles";
 	}
 	
